ba2ade141d25c5d6816c8bf68296bf3736cb0534
xize <glucassen@live.nl>
2013-12-02 16:18:25 +0100

    updated to 1.7.2

--- src/xize/biome2forest/Biome2Forest/biome2forest.java
+++ src/xize/biome2forest/Biome2Forest/biome2forest.java
@@ -5,8 +5,9 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.logging.Logger;
 
-import net.minecraft.server.v1_6_R3.BiomeBase;
-import net.minecraft.server.v1_6_R3.BiomeForest;
+import net.minecraft.server.v1_7_R1.BiomeBase;
+import net.minecraft.server.v1_7_R1.BiomeForest;
+import net.minecraft.server.v1_7_R1.BiomeTaiga;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,9 +26,10 @@ public class biome2forest extends JavaPlugin {
 	}
 
 	public void jungle() {
-        BiomeBase[] a = BiomeBase.biomes;
-        BiomeForest nb1 = new BiomeForest(21);
-        BiomeForest nb2 = new BiomeForest(22);
+        BiomeBase[] a = BiomeBase.n();
+        BiomeTaiga nb1 = new BiomeTaiga(21,1);
+        BiomeTaiga nb2 = new BiomeTaiga(22,1);
+        BiomeTaiga nb3 = new BiomeTaiga(23,0);
  
         try {
             Method m1 = BiomeBase.class.getMethod("b", int.class);
@@ -42,32 +44,41 @@ public class biome2forest extends JavaPlugin {
             Method m4 = BiomeBase.class.getMethod("a", float.class, float.class);
             m4.setAccessible(true);
  
-            m1.invoke(nb1, 353825);
+            m1.invoke(nb1, 5858897);
             m2.invoke(nb1, "Jungle");
             m3.invoke(nb1, 5159473);
-            m4.invoke(nb1, 0.7F, 0.8F);
+            m4.invoke(nb1, 0.3F, 0.8F);
  
-            m1.invoke(nb2, 353825);
+            m1.invoke(nb2, 4542270);
             m2.invoke(nb2, "JungleHills");
             m3.invoke(nb2, 5159473);
-            m4.invoke(nb2, 0.7F, 0.8F);
+            m4.invoke(nb2, 0.3F, 0.8F);
+            
+            m1.invoke(nb3, 747097);
+            m2.invoke(nb3, "JungleEdge");
+            m3.invoke(nb3, 5159473);
+            m4.invoke(nb3, 0.25F, 0.8F);
  
             Field f1 = BiomeBase.class.getDeclaredField("JUNGLE");
             Field f2 = BiomeBase.class.getDeclaredField("JUNGLE_HILLS");
+            Field f3 = BiomeBase.class.getDeclaredField("JUNGLE_EDGE");
             this.setFinalStatic(f1, nb1);
             this.setFinalStatic(f2, nb2);
+            this.setFinalStatic(f3, nb3);
  
             a[21] = nb1;
             a[22] = nb2;
+            a[23] = nb3;
  
-            Field f3 = BiomeBase.class.getDeclaredField("biomes");
-            this.setFinalStatic(f3, a);
+            Field BiomeField = BiomeBase.class.getDeclaredField("biomes");
+            this.setFinalStatic(BiomeField, a);
         } catch (Exception e) {}
     }
 	
 	public void ocean() {
-        BiomeBase[] a = BiomeBase.biomes;
-        BiomeForest nb1 = new BiomeForest(0);
+        BiomeBase[] a = BiomeBase.n();
+        BiomeForest nb1 = new BiomeForest(0,0);
+        BiomeForest nb2 = new BiomeForest(24, 2);
  
         try {
             Method m1 = BiomeBase.class.getMethod("b", int.class);
@@ -86,11 +97,20 @@ public class biome2forest extends JavaPlugin {
             m2.invoke(nb1, "Ocean");
             m3.invoke(nb1, 5159473);
             m4.invoke(nb1, 0.7F, 0.8F);
+            
+            m1.invoke(nb2, 353825);
+            m2.invoke(nb2, "Deep Ocean");
+            m3.invoke(nb2, 5159473);
+            m4.invoke(nb2, 0.7F, 0.8F);
  
             Field f1 = BiomeBase.class.getDeclaredField("OCEAN");
             this.setFinalStatic(f1, nb1);
+            
+            Field f2 = BiomeBase.class.getDeclaredField("DEEP_OCEAN");
+            this.setFinalStatic(f2, nb2);
  
             a[0] = nb1;
+            a[24] = nb2;
  
             Field f3 = BiomeBase.class.getDeclaredField("biomes");
             this.setFinalStatic(f3, a);
